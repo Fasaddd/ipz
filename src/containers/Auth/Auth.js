cfda327d8aa6970e37b97ec26fee76d0b5f62eba
Юра Прокопчук <yprokop1758@gmail.com>
2019-12-02 11:24:28 +0200

    adding backEnd and creating authorization

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,137 +1,180 @@
-import React, { Component } from 'react';
-import { withRouter } from 'react-router-dom';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
+import { connect } from "react-redux";
 
-import Button from '../../components/UI/Button/Button.js';
-import classes from './Auth.module.css';
+import Button from "../../components/UI/Button/Button.js";
+import classes from "./Auth.module.css";
 
-import * as actionTypes from '../../store/actions';
+import * as actionTypes from "../../store/actions";
+import Aux from "../../hoc/Auxillilary";
 
 class Auth extends Component {
+  state = {
+    info: {
+      email: null,
+      password: null
+    },
+    LogIn: true,
+    errorPass: null,
+    errorEmail: null,
+    emailValid: false,
+    passValid: false,
+    errorUserInfo: null
+  };
 
-    state = {
-        info: {
-            email: null,
-            password: null
+  onClickSubmitForm = e => {
+    e.preventDefault();
+    let LoginMode = this.state.LogIn;
+    let loginUser = { ...this.state.info };
+    let activeUser = loginUser.email;
+    if (LoginMode === true) {
+      console.log("Loginization");
+      let userInfo = { ...this.state.info };
+      let data = JSON.stringify(userInfo);
+      console.log(data);
+      fetch("/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
         },
-        LogIn: true,
-        error: null,
-        emailValid: false,
-        passValid: false
-    }
-
-
-
-    onClickSubmitForm = (e) => {
-        e.preventDefault();
-        let LoginMode = this.state.LogIn;
-        let loginUser = { ...this.state.info };
-        let activeUser = loginUser.email;
-        if (LoginMode === true) {
-            console.log('Loginization');
-            this.props.onSubmitForm(activeUser)
-            this.props.history.push('/main')
-        } else {
-            console.log('SignUp');
-            this.props.onSubmitForm(activeUser)
-            this.props.history.push('/main')
-        }
-
-
-        
-        // if (LoginMode === true) {
-        //     let temp;
-        //     temp = storeUser.some(el => {
-        //         return el.email.toLowerCase() === loginUser.email.toLowerCase() && el.password === loginUser.password
-        //     });
-        //     if (temp === false) {
-        //         this.setState({ Error: 'Email or password is incorrect! or Account is not exist' });
-        //     } else {
-        //         this.props.history.push('/tool');
-        //    }
-        // } else if (LoginMode === false) {
-        //     let temp;
-        //     temp = storeUser.some(el => {
-        //         return el.email.toLowerCase() === loginUser.email.toLowerCase()
-        //     });
-        //     if (temp === true) {
-        //         this.setState({ Error: 'Email is exist!' });
-        //     } else {
-        //         this.props.history.push('/tool')
-        //     };
-        // };
-    };
-
-    
-    onHandleInputChange = (event) => {
-        let name = event.target.name;
-        let value = event.target.value;
-        this.checkValidation(name, value);
-        let resInfo = {
-            ...this.state.info,
-            [name]: value
-        };
-        this.setState({
-            info: resInfo
+        body: data
+      }).then(response => {
+        response.json().then(body => {
+          console.log(body);
+          if (body.res == "Logged") {
+            this.props.onSubmitForm(activeUser);
+            this.props.history.push("/main");
+          }else{
+            this.setState({errorUserInfo: body.res});
+          }
         });
-    };
-
-
-    checkValidation = (name, value) => {
-        switch (name) {
-            case 'email':
-                let emailValid = value.match(/\w+@\w+\.\w+/);
-                emailValid ? this.setState({ error: null, emailValid: true }) : this.setState({ error: 'Invalid enter email', emailValid: false });
-                break;
-            case 'password':
-                let passValid = value.length >= 6
-                passValid ? this.setState({ error: null, passValid: true }) : this.setState({ error: 'Password is to short', passValid: false });
-                break;
-            default:
-                break;
-        };
+      });
+    } else {
+      console.log("SignUp");
+      let userInfo = { ...this.state.info };
+      let data = JSON.stringify(userInfo);
+      console.log(data);
+      fetch("/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: data
+      }).then(response => {
+        response.json().then(body => {
+          if (body.result == "INSERT") {
+            this.props.onSubmitForm(activeUser);
+            this.props.history.push("/main");
+          } else {
+            this.setState({ errorUserInfo: body.result });
+          }
+        });
+      });
     }
+  };
 
-    onClickChangeSign = () => {
-        this.setState({ LogIn: !this.state.LogIn })
-    }
-
-    render() {
-        let disabledButt = true;
-        if (this.state.passValid === true && this.state.emailValid === true) {
-            disabledButt = false;
-        }
-        let error = null;
-        if (this.state.error != null) {
-            error = <p style={{ 'color': 'red', 'fontSize': '25px' }}>{this.state.error}</p>
-        }
-        return (
-            <div className={classes.Auth}>
-                {this.state.LogIn ? 'LogIn' : 'SignIN'}
-                <form>
-                    <input type='email' name="email" onChange={this.onHandleInputChange} placeholder='test@test.com' />
-                    <input type="password" name="password" onChange={this.onHandleInputChange} placeholder='1234567' />
-                    {error}
-                    <Button
-                        // disabled={disabledButt}
-                        clicked={this.onClickSubmitForm}
-                        btnType="Success">Submit</Button>
-                </form>
-                <Button
-                    btnType="Danger"
-                    clicked={this.onClickChangeSign}
-                >SWITCH TO {this.state.LogIn ? 'SignIn' : 'LogIn'} </Button>
-            </div>
-        );
+  onHandleInputChange = event => {
+    let name = event.target.name;
+    let value = event.target.value;
+    this.checkValidation(name, value);
+    let resInfo = {
+      ...this.state.info,
+      [name]: value
     };
-};
+    this.setState({
+      info: resInfo
+    });
+  };
 
+  checkValidation = (name, value) => {
+    switch (name) {
+      case "email":
+        let emailValid = value.match(/\w+@\w+\.\w+/);
+        emailValid
+          ? this.setState({ errorEmail: null, emailValid: true })
+          : this.setState({
+            errorEmail: "Invalid enter email",
+            emailValid: false
+          });
+        break;
+      case "password":
+        console.log(value.length);
+        let passValid = value.length >= 6;
+        passValid
+          ? this.setState({ errorPass: null, passValid: true })
+          : this.setState({
+            errorPass: "Password is to short",
+            passValid: false
+          });
+        break;
+      default:
+        break;
+    }
+  };
+
+  onClickChangeSign = () => {
+    this.setState({ LogIn: !this.state.LogIn });
+  };
+
+  render() {
+    let disabledButt = true;
+    if (this.state.passValid === true && this.state.emailValid === true) {
+      disabledButt = false;
+    }
+    let error = null;
+    if (this.state.errorEmail != null || this.state.errorPass != null) {
+      error = (
+        <Aux>
+          <p style={{ color: "red", fontSize: "25px" }}>
+            {this.state.errorEmail}
+          </p>
+          <p style={{ color: "red", fontSize: "25px" }}>
+            {this.state.errorPass}
+          </p>
+        </Aux>
+      );
+    }
+    return (
+      <div className={classes.Auth}>
+        <div style={{ color: "red", fontSize: "20px" }}>
+          {this.state.errorUserInfo}
+        </div>
+        {this.state.LogIn ? "LogIn" : "SignIN"}
+        <form>
+          <input
+            type="email"
+            name="email"
+            onChange={this.onHandleInputChange}
+            placeholder="test@test.com"
+          />
+          <input
+            type="password"
+            name="password"
+            onChange={this.onHandleInputChange}
+            placeholder="1234567"
+          />
+          {error}
+          <Button
+            disabled={disabledButt}
+            clicked={this.onClickSubmitForm}
+            btnType="Success"
+          >
+            Submit
+          </Button>
+        </form>
+        <Button btnType="Danger" clicked={this.onClickChangeSign}>
+          SWITCH TO {this.state.LogIn ? "SignIn" : "LogIn"}
+        </Button>
+      </div>
+    );
+  }
+}
 
 const mapDispatchToProps = dispatch => {
-    return {
-        onSubmitForm: (user) => dispatch({ type: actionTypes.AUTHENTICATED_STATUS, activeUser: user })
-    };
+  return {
+    onSubmitForm: user =>
+      dispatch({ type: actionTypes.AUTHENTICATED_STATUS, activeUser: user })
+  };
 };
 
-
-export default connect( null,mapDispatchToProps)(withRouter(Auth));
+export default connect(null, mapDispatchToProps)(withRouter(Auth));
---
+++ src/server/server.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const port = process.env.PORT || 5000;
+
+const { Pool, Client } = require("pg");
+const connectionString =
+  "postgressql://postgres:yura1758@localhost:1758/beauty-app";
+
+const client = new Client({
+  connectionString: connectionString
+});
+
+const app = express();
+
+// PostgresSQL
+client.connect();
+
+const jsonParser = express.json();
+
+// REGISTER
+app.post("/register", jsonParser, function(request, response) {
+  const data = {email: request.body.email, password: request.body.password};
+  console.log(data);
+  let result = false;
+  client.query("SELECT * from users", (err, res) => {
+      console.log(res.rows);
+    if (err) {
+      console.log(err);
+      response.status(400).json(err);
+    } else {
+      res.rows.forEach(el => {
+        if (el.login == data.email) {
+          response.status(200).json({ result: "Login exist!" });
+          result = true;
+          return;
+        }
+      });
+      if (!result) {
+        console.log(request.body.password, request.body.password)
+        client.query(
+          'INSERT INTO users(login, password) VALUES ($1, $2)', [data.email, data.password], 
+          (err, res) => {
+            console.log(res);
+            response.json({ result: "INSERT" });
+            return;
+          }
+        );
+      }
+    }
+  });
+});
+
+ 
+
+// LOGIN
+app.post("/login", jsonParser, function(request, response) {
+  const data = {email: request.body.email, password: request.body.password};
+  let exist = false;
+  client.query("SELECT * from users", (err, res) => {
+      console.log(res.rows);
+    if (err) {
+      return response.json(err);
+    } else {
+      res.rows.forEach(el => {
+        if(el.login === data.email && el.password === data.password){
+          response.json({res: "Logged"});
+          exist = true;
+        }
+      })
+      if(!exist){
+        response.json({res:"Account is not exist or incorrect"});
+      }
+    }
+  })
+});
+
+  app.listen(port, () => console.log(`Listening on port ${port}`));
